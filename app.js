1a64584e40a5300e2f41c6ed087323e83ee05cd6
MichalKaniowski <99476197+MichalKaniowski@users.noreply.github.com>
2022-12-01 18:39:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import { renderDashboard } from "./view-dashboard.js";
 import { renderDetails } from "./view-details.js";
 
 
-
 if (window.location.search.includes("?country=")) {
     document.querySelector(".filters").classList.add("hidden");
     renderDetails();
